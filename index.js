b0ca35a7e69166a180f9bbca8a17dce6f313a586
arditkukaj <arditkukaj4@gmail.com>
2023-10-03 09:56:31 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(array, string){
+    const gifts = []
+    for(let i = 0 ; i < array.length; i++){
+    gifts.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return gifts;
+}
+
+
+function countDown(){
+
+const countdown=function(i) {
+    console.log(i);
+    i>0 && countdown(i-1);
+}
+    countdown(10);
+}
